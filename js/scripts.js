a93a56e33ad3e8de5b81a981944f8898bca088db
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-20 19:56:53 +0300

    -Add Objects to PizzaCrust

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,9 @@ let medium = new PizzaSize("Medium", 800);
 let large = new PizzaSize("Large", 1200);
 
 // Add objects to PizzaCrust Constructor
-let pepporoni = new PizzaCrust("Pepporoni", 100);
-let mushroom = new PizzaCrust("Mushroom", 150);
-let bacon = new PizzaCrust("bacon");
+let stuffed = new PizzaCrust("Stuffed", 100);
+let crispy = new PizzaCrust("Crispy", 150);
+let glutten = new PizzaCrust("Glutten-free");
 
 let orderBtn = document.getElementById("order-button");
 orderBtn.addEventListener('click', displayOrderSummary);
