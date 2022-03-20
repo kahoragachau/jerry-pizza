32bb4410e2fe980b498a818e46cc5b1ed70516da
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-20 19:41:53 +0300

    -Add pizzaCrust constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 // Business Logic
 function PizzaSize(size, price){
     this.size = size;
-    this.price = price
+    this.price = price;
 }
 
+function PizzaCrust(crust, price){
+    this.crust = crust;
+    this.price = price;
+}
 
 let orderBtn = document.getElementById("order-button");
 orderBtn.addEventListener('click', displayOrderSummary);
