56791de9b2d50840b84bb96c4ce48884676350ee
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-20 19:48:31 +0300

    -Add Objects to PizzaSize

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,12 @@ function PizzaTopping(topping, price){
     this.topping = topping;
     this.price = price;
 }
+
+// Add objects to PizzaSize Constructor
+let small = new PizzaSize("Small", 400);
+let medium = new PizzaSize("Medium", 800);
+let large = new PizzaSize("Large", 1200);
+
 let orderBtn = document.getElementById("order-button");
 orderBtn.addEventListener('click', displayOrderSummary);
 
