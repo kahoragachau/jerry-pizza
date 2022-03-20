e0565f4e13d65dc60c9de2c2ddbd21547d5dd85f
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-20 22:46:37 +0300

    -Add License

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,5 @@ $(document).ready(function(){
         let pizzaSize = $("#size option:selected").val();
         let pizzaCrust = $("#crusts option:selected").val();
         let pizzaTopping = $("#toppings option:selected").val();
-        if (pizzaSize === small.size){
-            console.log(small.price);
-        }
-        if (pizzaTopping === pepporoni.topping){
-            console.log(pepporoni.price)
-        }
-        if (pizzaCrust === stuffed.crust){
-            console.log(stuffed.price);
-        }
     });
 })
