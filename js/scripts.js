a99e0addc66d2977816f3dd241951e4ce377d304
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-20 20:00:32 +0300

    -Add Objects to PizzaTopping

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,12 @@ let large = new PizzaSize("Large", 1200);
 // Add objects to PizzaCrust Constructor
 let stuffed = new PizzaCrust("Stuffed", 100);
 let crispy = new PizzaCrust("Crispy", 150);
-let glutten = new PizzaCrust("Glutten-free");
+let glutten = new PizzaCrust("Glutten-free", 200);
+
+// Add objects to PizzaTopping Constructor
+let pepporoni = new PizzaTopping("Pepporoni", 50);
+let mushroom = new PizzaTopping("Mushroom", 100);
+let bacon = new PizzaTopping("Bacon", 150);
 
 let orderBtn = document.getElementById("order-button");
 orderBtn.addEventListener('click', displayOrderSummary);
